45fe10ae5622ee29206e601b958a9b7f15afcb3e
Kynan Stewart Hughes <mail@kynan.net.au>
2021-08-19 18:17:40 +1000

    ...

--- public/js/Bouddi.js
+++ public/js/Bouddi.js
@@ -43,9 +43,9 @@ const [styles] = createStyles({
     justifyContent: "center",
   },
   map: {
-	  width: "640px",
-	  height: "480px",
-  }
+    width: `${window.innerWidth}px`,
+    height: `${window.innerHeight}px`,
+  },
 });
 
 /*::
@@ -54,23 +54,40 @@ type Props = {
 };
 */
 const Bouddi = (props /*: Props */) /*: string */ => {
-
   useEffect(() => {
-	// $FlowFixMe
-	const map = L.map('map', {
-		crs: L.CRS.Simple
-	});
+    // $FlowFixMe
+    const map = L.map("map", {
+      crs: L.CRS.Simple,
+    });
 
-	const bounds = [[0,0], [1000,1000]];
-	const image = L.imageOverlay('/img/uqm_map_full.png', bounds).addTo(map);
+    const bounds = [
+      [0, 0],
+      [1000, 1000],
+    ];
+    const image = L.imageOverlay("/img/map_bouddi.png", bounds).addTo(map);
 
-	map.fitBounds(bounds);
+    map.fitBounds(bounds);
+
+    var preschool = L.latLng([290, 340]);
+    L.marker(preschool).addTo(map);
+    var school1 = L.latLng([295, 295]);
+    L.marker(school1).addTo(map);
+    var school2 = L.latLng([325, 300]);
+    L.marker(school2).addTo(map);
+    var house1 = L.latLng([515, 55]);
+    L.marker(house1).addTo(map);
+    var house2 = L.latLng([575, 35]);
+    L.marker(house2).addTo(map);
+    var house3 = L.latLng([420, 500]);
+    L.marker(house3).addTo(map);
+    var house4 = L.latLng([380, 515]);
+    L.marker(house4).addTo(map);
   }, []);
 
   // console.log(props.count.isInteger());
   return html`
     <div className="${styles.container}">
-		<div id='map' className="${styles.map}"></div>
+      <div id="map" className="${styles.map}"></div>
     </div>
   `;
 };
